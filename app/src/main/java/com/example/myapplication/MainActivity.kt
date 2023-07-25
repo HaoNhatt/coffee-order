3ae8d373b3f2ae6cdbbb59450c8edc14ae4290e2
Hào Nhật <haonhatht@gmail.com>
2023-07-25 22:51:17 +0700

    Fix bugs input chat

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,21 +1,37 @@
 package com.example.myapplication
 
-import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.TextView.OnEditorActionListener
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
 //    var myCart =
+//    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+//        if (event.action == MotionEvent.ACTION_DOWN) {
+//            val v = currentFocus
+//            if (v is EditText) {
+//                val outRect = Rect()
+//                v.getGlobalVisibleRect(outRect)
+//                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+//                    Log.d("focus", "touchevent")
+//                    v.clearFocus()
+//                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+//                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+//                }
+//            }
+//        }
+//        return super.dispatchTouchEvent(event)
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -106,7 +122,53 @@ class MainActivity : AppCompatActivity() {
 
     fun goToProfile(view: View) {
         setContentView(R.layout.profile)
+        val editName = findViewById<EditText>(R.id.user_name)
+        editName.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                //Clear focus here from edittext
+                editName.clearFocus()
+                val newName = editName.text.toString()
+                Log.d("HaoNhatTest", "$newName")
+            }
+            false
+        }
+        val editPhone = findViewById<EditText>(R.id.user_phone)
+        editPhone.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                //Clear focus here from edittext
+                editPhone.clearFocus()
+                val newPhone = editPhone.text.toString()
+                Log.d("HaoNhatTest", "$newPhone")
+            }
+            false
+        }
+        val editEmail = findViewById<EditText>(R.id.user_email)
+        editEmail.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                //Clear focus here from edittext
+                editEmail.clearFocus()
+                val newEmail = editName.text.toString()
+                Log.d("HaoNhatTest", "$newEmail")
+            }
+            false
+        }
+        val editAddress = findViewById<EditText>(R.id.user_address)
+        editName.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                //Clear focus here from edittext
+                editName.clearFocus()
+                val newAddress = editName.text.toString()
+                Log.d("HaoNhatTest", "$newAddress")
+            }
+            false
+        }
     }
+
+//    fun releaseFocus(view: View) {
+//        Log.d("HaoNhatTest", "$currentFocus")
+//        currentFocus?.clearFocus()
+//        Log.d("HaoNhatTest", "$currentFocus")
+//    }
 
     fun goToReward(view: View) {
 
@@ -142,28 +204,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun focusEditText(view: View) {
-
-//        findViewById<RelativeLayout>(R.id.layout_profile).dispatchTouchEvent() { _, event ->
-//            val v = currentFocus
-//            if (v != null) {
-//                v.clearFocus()
-//                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-//                imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
-//            }
-//            return super.dispatchTouchEvent(event)
-//        }
         when (view) {
             findViewById<ImageButton>(R.id.edit_name) -> {
                 val editName = findViewById<EditText>(R.id.user_name)
                 editName.setText("")
                 editName.requestFocus()
                 showSoftKeyboard(editName)
-                editName.setOnFocusChangeListener { _, hasFocus ->
-                    if (!hasFocus) {
-                        val newName = editName.text.toString()
-                        Log.d("User Name","$newName")
-                    }
-                }
             };
             findViewById<ImageButton>(R.id.edit_phone) -> {
                 val editPhone = findViewById<EditText>(R.id.user_phone)
@@ -186,7 +232,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun showSoftKeyboard(view: View) {
+    private fun showSoftKeyboard(view: View) {
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
     }
