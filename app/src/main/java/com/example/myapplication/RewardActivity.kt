65dfffeed2f1962c517cc54bba07f11455a2c093
Hào Nhật <haonhatht@gmail.com>
2023-07-28 15:42:52 +0700

    Add remaining screen, almost done except recycler view

--- app/src/main/java/com/example/myapplication/RewardActivity.kt
+++ app/src/main/java/com/example/myapplication/RewardActivity.kt
@@ -1,11 +1,26 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class RewardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reward)
+    }
+
+    fun actionGoToRedeem(view: View) {
+        val intent = Intent(this, RedeemActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToHomePage(view: View) {
+        finish()
+    }
+
+    fun goToMyOrders(view: View) {
+        finish()
     }
 }
